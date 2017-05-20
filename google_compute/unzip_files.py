f165082f8b76428c99709b941b22e9234a61708b
Anne Rogers <amr@cs.uchicago.edu>
2017-05-20 14:58:17 -0500

    unzip only 2016

--- google_compute/unzip_files.py
+++ google_compute/unzip_files.py
@@ -2,6 +2,10 @@
 # to unzip all of the compressed comments data
 import os
 
+# this code was originally used to unzip 300GB+ of compressed files
+# scrapped because now we are only using a subset and want to unzip
+# only one folder
+'''
 folders = []
 IGNORE = ['lost+found', 'unzip_files.py', '2006', '2010', '2009']
 EXEC = "bzip2 -d "
@@ -24,3 +28,17 @@ for folder in folders:
     print('DONE WITH ' + folder + ' GOING BACK TO MAIN DIRECTORY')
     os.chdir('..')
     print('NOW BACK IN ' + os.getcwd())
+'''
+
+EXEC = "bzip2 -d "
+
+for bz2 in os.listdir():
+    print('WORKING ON ' + bz2)
+    if '.bz2' in bz2:
+        os.system(EXEC + bz2)
+        print('FINISHED UNCOMPRESSING ' + bz2)
+    else:
+        print('DROPPED ' + bz2)
+
+print('DONE')
+
