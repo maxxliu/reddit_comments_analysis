83e0a02e4d2cfe618280c03a5301c15571e80135
Max Liu <Max@wireless-s1-no-150-26-157.uchicago.edu>
2017-05-21 22:53:43 -0500

    mr for stocks outline done, waiting for sent

--- testing_scripts/mr_for_stocks.py
+++ testing_scripts/mr_for_stocks.py
@@ -15,14 +15,33 @@ class DailySentiment(MRJob):
             if word in json_v['body']:
                 # do the sentiment analysis
                 # should return sentiment and word count
-                
+                key = word + json_v['suffix']
+                yield key, values # (sentiment, word_count)
 
 
-        # do something
     def combiner(self, key, values):
-        # do something
+        sent_count = 0 # number of values
+        sent_total = 0 # aggregate of sentiment scores
+        word_count = 0 # number of words
+        for value in values:
+            sent_count += 1
+            sent_total += values[0]
+            word_count += values[1]
+
+        yield key, (sent_count, sent_total, word_count)
+
+
     def reducer(self, key, values):
-        # do something
+        sent_count = 0
+        sent_total = 0
+        word_count = 0
+        for value in values:
+            sent_count += values[0]
+            sent_total += values[1]
+            word_count += values[2]
+        sent_avg = sent_total / sent_count
+
+        yield key, (sent_avg, sent_total, word_count)
 
 if __name__=='__main__':
     DailySentiment.run()
