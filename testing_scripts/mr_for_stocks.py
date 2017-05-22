8a609327a0d70529013a74003a7cfbbe5ddf8b96
Max Liu <Max@wireless-s1-no-150-26-157.uchicago.edu>
2017-05-21 20:20:29 -0500

    progress

--- testing_scripts/mr_for_stocks.py
+++ testing_scripts/mr_for_stocks.py
@@ -5,12 +5,18 @@ import json
 import time
 import nltk_testing
 
+dates = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05',
+            'Jun': '06', 'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10',
+            'Nov': '11', 'Dec': '12'}
+
 class DailySentiment(MRJob):
     def mapper(self, _, line):
         json_v = json.loads(line)
         epoch = json_v['created_utc']
         json_v['time'] = time.strftime("%a %d %b %Y %H:%M:%S", time.localtime(epoch))
+        day = json_v['time'][4:6]
         
+
         # do something
     def combiner(self, key, values):
         # do something
