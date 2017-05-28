4c4ad5114be61212932c6df46367d3289bcfd75a
Anne Rogers <amr@cs.uchicago.edu>
2017-05-28 17:03:36 -0500

    textblob sent analysis file

---
+++ testing_scripts/tb_sentiment.py
@@ -0,0 +1,24 @@
+import nltk.tokenize
+from textblob import TextBlob
+
+def comment_analysis(key_word, comment):
+    '''
+    '''
+    comment_analysis = {'mentions': 0, 'score': 0}
+    scores = []
+    sent_list = nltk.sent_tokenize(comment)
+    
+    for sent in sent_list:
+        if word_in(key_word, sent):
+            comment_analysis['mentions'] += 1
+            analysis = TextBlob(sent)
+            scores.append(analysis.sentiment.polarity)
+
+    comment_analysis['score'] = sum(scores) / len(scores)
+
+    return comment_analysis
+
+
+def word_in(key_word, sent):
+    if key_word in sent:
+        return True
