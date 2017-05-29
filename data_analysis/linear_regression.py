44928ec6d0fa4890a6463950c920328d8a1f0244
Max Liu <Max@wireless-s1-no-150-30-103.uchicago.edu>
2017-05-28 20:08:16 -0500

    discard

--- data_analysis/linear_regression.py
+++ data_analysis/linear_regression.py
@@ -1,3 +1,9 @@
 import re
 
 pattern = '[0-9]+\.[0-9]+|[0-9]+'
+
+def make_data_struct(txt_file):
+    '''
+    makes a 
+    txt_file (text file): text file with the results from sentiment analysis
+    '''
