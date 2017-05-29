42432202b01b7a3c989ec132c82fc75142909c9b
Anne Rogers <amr@cs.uchicago.edu>
2017-05-28 21:23:43 -0500

    related words fn added

--- testing_scripts/tb_sentiment.py
+++ testing_scripts/tb_sentiment.py
@@ -1,5 +1,8 @@
 import nltk.tokenize
+from nltk.corpus import stopwords
 from textblob import TextBlob
+
+RELATED_WORD_TAGS = ['NN', 'NNS', 'NNP', 'NNPS', 'FW']
 
 
 def comment_analysis(key_word, comment):
@@ -18,6 +21,20 @@ def comment_analysis(key_word, comment):
     comment_analysis['score'] = sum(scores) / len(scores)
 
     return comment_analysis
+
+
+def related_words(key_word, comment):
+    '''
+    '''
+    related = {}
+    word_list = nltk.word_tokenize(comment)
+    tagged_words = nltk.pos_tag(word_list)
+
+    for info in tagged_words:
+        if info[1] in RELATED_WORD_TAGS and info[0] != key_word:
+            related[info[0]] = related.get(info[0], 0) + 1
+
+    return related 
 
 
 def dumb_analysis(comment):
