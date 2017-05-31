97163c525575f441eae73a5a94283f03470ffbbb
Max Liu <Max@Maxs-MacBook-Pro-2.local>
2017-05-31 01:28:48 -0500

    restructuring dataframe creation

--- data_analysis/dataframe.py
+++ data_analysis/dataframe.py
@@ -12,13 +12,15 @@ def top_companies(txt_file):
     '''
     txt = open(txt_file)
     d = {}
+    companies = []
 
     for line in txt:
         words = re.findall(PATTERN, line)
         key = words.pop(0)
         d[key] = words
+        companies.append(key)
 
-    return d
+    return d, companies
 
 def training_data(txt_file, csv_file):
     '''
@@ -28,6 +30,7 @@ def training_data(txt_file, csv_file):
                         'products': [iPhone, iPod]}
     '''
     pass
+
 
 def read_sent_file(txt_file):
     '''
@@ -58,17 +61,62 @@ def read_sent_file(txt_file):
     return data_dict
 
 
-def make_data_struct(data_dict, training_set, typ, date, quarter):
+def single_dp(data_dict, training_set, companies, typ, date, quarter):
+    '''
+    makes single data column with points of interest
+
+    data_dict (dict) - dictionary of data points
+    training_set (dict) - dictionary of companies with their top products and their
+                    quarterly earning performance
+    companies (list) - list of the top 100 companies
+    typ (str) - using month or week?
+    date (int) - which month or week?
+    quarter (int) - which quarter are we analyzing?
+    '''
+    averages = []
+    counts = []
+
+    for company in companies:
+        count = 0
+        total = 0
+
+        for item in training_set[company]['products']:
+            count += data_dict[item][typ][date][0]
+            total += data_dict[item][typ][date][1]
+
+        averages.append(total / count)
+        counts.append(total)
+
+    avg_key = typ + int(date) + 'avg'
+    count_key = typ + int(date) + 'cnt'
+    temp_df = pd.DataFrame({avg_key: averages, count_key: counts})
+
+    return temp_df
+
+
+def mk_data_strct(data_dict, training_set, companies, m_r, w_r, quarter):
     '''
     makes a data struct with points of interest
 
     data_dict (dict) - dictionary of data points
     training_set (dict) - dictionary of companies with their top products and their
                     quarterly earning performance
-    typ (str) - using month or week?
-    date (int) - which month or week?
+    companies (list) - list of the top 100 companies
+    m_r (tuple) - range of months to look at
+    w_r (tuple) - range of weeks to look at
     quarter (int) - which quarter are we analyzing?
     '''
+    
+
+
+
+
+
+
+
+
+
+
     x = []
     y = []
     for key, value in training_set.items():
