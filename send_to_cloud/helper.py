f4c12370f889149208ef8c9f5b2652c461df4c80
Max Liu <Max@wireless-s1-no-150-17-88.uchicago.edu>
2017-05-31 09:37:55 -0500

    to send to dataproc when we get it running

---
+++ send_to_cloud/helper.py
@@ -0,0 +1,81 @@
+import nltk.tokenize
+from nltk.corpus import stopwords
+from textblob import TextBlob
+import json
+import time
+import datetime
+
+
+PATTERN = '[a-zA-Z]+'
+
+
+DATES = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05',
+            'Jun': '06', 'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10',
+            'Nov': '11', 'Dec': '12'}
+
+
+STOPWORDS = set(stopwords.words('english'))
+RELATED_WORD_TAGS = ['NN', 'NNS', 'NNP', 'NNPS', 'FW']
+
+
+def clean_line(raw_line):
+    '''
+    cleans raw line from data set
+
+    raw_line (string) - json string
+    '''
+    json_v = json.loads(raw_line)
+    epoch = json_v['created_utc']
+    json_v['time'] = time.strftime("%a %d %b %Y %H:%M:%S", time.localtime(epoch))
+    # day of the week, day, month, year, time
+    day = json_v['time'][4:6]
+    month = json_v['time'][7:10]
+    year = json_v['time'][11:15]
+    n_mnth = DATES[month]
+    week_num = datetime.date(int(year), int(n_mnth), int(day)).isocalendar()[1]
+    json_v['week'] = str(week_num)
+    json_v['month'] = n_mnth
+    json_v['year'] = year
+    comment = json_v['body']
+    comment = comment.replace('\n', '')
+    comment = comment.replace('\r', '')
+    json_v['body'] = comment
+
+    return json_v
+
+
+def product_sentiment(product, comment):
+    '''
+    tells us the sentiment towards a product word we are intersted in
+
+    product (string) - word that we are looking at sentiment for
+    comment (string) - the reddit comment
+    '''
+    mentions = 0
+    total_sentiment = 0
+    sent_list = nltk.sent_tokenize(comment)
+
+    for sent in sent_list:
+        if product in sent:
+            mentions += 1
+            analysis = TextBlob(sent)
+            total_sentiment += analysis.sentiment.polarity
+
+    return mentions, total_sentiment
+
+
+def related_words(key_word, to_use):
+    '''
+    finds all of the related words in a string
+
+    key_word (string) - word that we are looking for related words for
+    to_use (list) - list of relevant words
+    '''
+    related = {}
+    tagged_words = nltk.pos_tag(to_use)
+
+    for info in tagged_words:
+        if info[1] in RELATED_WORD_TAGS and info[0] != key_word:
+            related[info[0]] = related.get(info[0], 0) + 1
+
+    return related
---
+++ send_to_cloud/top_companies.py
@@ -0,0 +1,88 @@
+from mrjob.job import MRJob
+import re
+import json
+import helper
+
+
+COMP_LIST = []
+
+
+def load_lst(csv_file):
+    '''
+    csv_file (csv) - file with the largest 1000 companies
+    '''
+    csv = open(csv_file)
+    for i in csv:
+        i = i.strip('\n')
+        COMP_LIST.append(i)
+    COMP_LIST.pop(0)
+
+
+class t_companies_baskets(MRJob):
+    '''
+    yields 3 lists of commenters:
+    1. Commenters with highest upvote counts
+    2. Commenters that are the meanest
+    3. Commenters that are the nicest
+    '''
+
+
+    def mapper(self, _, line):
+        j = helper.clean_line(line)
+
+        comment = j['body']
+        words = re.findall(helper.PATTERN, comment)
+        words = set(words)
+        to_use = [x for x in words if x not in helper.STOPWORDS]
+
+        for word in to_use:
+            if word in COMP_LIST:
+                related_dict = helper.related_words(word, to_use)
+                yield word, (1, related_dict)
+
+
+    def combiner(self, company, count_dict_tuple):
+        count = 0
+        related_dict = {}
+        for tup in count_dict_tuple:
+            count += tup[0]
+            for word, values in tup[1].items():
+                if word not in related_dict:
+                    related_dict[word] = 0
+                related_dict[word] += values
+        yield company, (count, related_dict)
+
+
+    def reducer_init(self):
+        self.companies = [None] * 100
+        self.comp_count = [0] * 100
+        self.related_words = [None] * 100
+
+
+    def reducer(self, company, count_dict_tuple):
+        count = 0
+        related_dict = {}
+        for tup in count_dict_tuple:
+            count += tup[0]
+            for word, values in tup[1].items():
+                if word not in related_dict:
+                    related_dict[word] = 0
+                related_dict[word] += values
+        u = 99
+        while count > self.comp_count[u] and u > -1:
+            u = u - 1
+        if u != 99:
+            self.companies.insert((u + 1), company)
+            self.comp_count.insert((u + 1), count)
+            sorted_words = sorted(related_dict, key=related_dict.get, reverse=True)
+            self.related_words.insert((u + 1), sorted_words[0:5])
+
+
+    def reducer_final(self):
+        for i in range(100):
+            yield self.companies[i] ,self.related_words[i]
+
+
+if __name__ == '__main__':
+    load_lst('companylist.csv')
+    t_companies_baskets.run()
