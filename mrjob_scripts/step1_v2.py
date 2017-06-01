f92cd01c4de56e945a3fca698213bd906444273a
Max Liu <Max@wireless-s1-no-150-29-94.uchicago.edu>
2017-06-01 17:22:59 -0500

    rewrote into steps

--- mrjob_scripts/step1_v2.py
+++ mrjob_scripts/step1_v2.py
@@ -3,6 +3,7 @@ import re
 import json
 from textblob import TextBlob
 import os
+from mrjob.step import MRStep
 
 '''
 this mapreduce DOES NOT require the helper.py file
@@ -14,7 +15,7 @@ class t_companies_baskets(MRJob):
     '''
     finds the top 100 companies and their top mentioned words
     '''
-    def mapper_init(self):
+    def mapper_init_1(self):
 
         self.comp_list = set()
         self.stopwords = set()
@@ -32,7 +33,7 @@ class t_companies_baskets(MRJob):
             self.stopwords.add(s)
 
 
-    def mapper(self, _, line):
+    def mapper_1(self, _, line):
         json_v = json.loads(line)
         comment = json_v['body']
         comment = comment.replace('\n', '')
@@ -57,7 +58,7 @@ class t_companies_baskets(MRJob):
                 yield word, (1, related)
 
 
-    def combiner(self, company, count_dict_tuple):
+    def combiner_1(self, company, count_dict_tuple):
         count = 0
         related_dict = {}
         for tup in count_dict_tuple:
@@ -69,13 +70,13 @@ class t_companies_baskets(MRJob):
         yield company, (count, related_dict)
 
 
-    def reducer_init(self):
+    def reducer_init_1(self):
         self.companies = [None] * 100
         self.comp_count = [0] * 100
         self.related_words = [None] * 100
 
 
-    def reducer(self, company, count_dict_tuple):
+    def reducer_1(self, company, count_dict_tuple):
         count = 0
         related_dict = {}
         for tup in count_dict_tuple:
@@ -94,9 +95,39 @@ class t_companies_baskets(MRJob):
             self.related_words.insert((u + 1), sorted_words[0:5])
 
 
-    def reducer_final(self):
-        for i in range(100):
-            yield self.companies[i] ,self.related_words[i]
+    def reducer_final_1(self):
+        yield None, (self.comp_count, self.companies, self.related_words)
+
+
+    def reducer_concat(self, _, values):
+        final_comp = [(0, 0, 0)] * 100
+
+        for value in values:
+            for i in range(100):
+                cur = 99
+                temp = (value[0][i], value[1][i], value[2][i])
+                while cur > 0:
+                    if temp[0] > final_comp[cur][0]:
+                        rm = final_comp.pop(cur)
+                        final_comp.insert(cur, temp)
+                        temp = rm
+                    cur -= 1
+
+
+        for item in final_comp:
+            yield item[1], item[2]
+
+
+    def steps(self):
+        return [
+            MRStep(mapper_init=self.mapper_init_1,
+                    mapper=self.mapper_1,
+                    combiner=self.combiner_1,
+                    reducer_init=self.reducer_init_1,
+                    reducer=self.reducer_1,
+                    reducer_final=self.reducer_final_1),
+            MRStep(reducer=self.reducer_concat)
+        ]
 
 
 if __name__ == '__main__':
